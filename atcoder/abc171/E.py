acfc8d278d1d7b58483f27fa3a5f823f188c1772
Siddhant <siddhantkhare2694@gmail.com>
2020-06-21 18:18:00 +0530

    Add solution for problem `E - Red Scarf` Link: https://atcoder.jp/contests/abc171/tasks/abc171_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc171/submissions/14567035

---
+++ atcoder/abc171/E.py
@@ -0,0 +1,16 @@
+n = int(input())
+a = list(map(int, input().split()))
+ans = 0
+for i in range(n):
+    ans ^= a[i]
+for i in range(n):
+    print(ans^a[i], end=' ')
+print()
+"""
+for i in range(n):
+    ans = 0
+    for j in range(n):
+        if i != j:
+            ans ^= a[j]
+    print(ans)
+""" 
