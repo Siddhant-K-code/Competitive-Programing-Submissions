30b826d04bbed7badd1aa70175c5befe1a970baf
Siddhant <siddhantkhare2694@gmail.com>
2021-01-16 18:08:00 +0530

    Add solution for problem `B - Mex Boxes` Link: https://atcoder.jp/contests/keyence2021/tasks/keyence2021_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/keyence2021/submissions/19468450

---
+++ atcoder/keyence2021/B.cpp
@@ -0,0 +1,101 @@
+/*
+ * Author : Siddhant Khare
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define ll long long
+#define ld long double
+#define arr array
+#define si(t) scanf("%d", &t)
+#define sl(t) scanf("%ld", &t)
+#define sll(t) scanf("%lld", &t)
+#define sc(t) scanf("%c", &t)
+#define ss(t) scanf("%s", &t)
+#define sf(t) scanf("%f", &t)
+#define slf(t) scanf("%lf", &t)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%ld\n", x)
+#define pll(x) printf("%lld\n", x)
+#define pf(x) printf("%f")
+#define sz(x) (int)(x).size()
+#define mem(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(x) x.begin(), x.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define sortall(x) sort(all(x))
+#define mp make_pair
+#define pb push_back
+#define PI 3.1415926535897932384626433832795
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define read(type) readInt<type>()
+const double pi = acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<ll> VLL;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+/**************************************/
+
+/******** User-defined Function *******/
+void solve()
+{
+    ll a, b;
+    sll(a);
+    sll(b);
+    // Algorithm
+
+    // Output
+    // pll(ans) // long long print
+}
+
+/**************************************/
+
+/********** Main()  function **********/
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int N, K, temp, count = 0;
+    cin >> N >> K;
+    int arr[300000] = {0};
+    for (int i = 0; i < N; i++)
+    {
+        cin >> temp;
+        arr[temp]++;
+    }
+    for (int i = 0; i < K; i++)
+    {
+        for (int j = 0; j < 300000; j++)
+        {
+            if (arr[j] > 0)
+            {
+                count++;
+                arr[j]--;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    cout << count;
+}
+/********  Main() Ends Here *************/
