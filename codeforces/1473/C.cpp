7ddb2d0fa16ab23b2f1150472c3dca7d416b0ab4
Siddhant <siddhantkhare2694@gmail.com>
2021-01-17 12:43:00 +0530

    Add solution for problem `C - No More Inversions` Link: https://codeforces.com/contest/1473/problem/C Tags: constructive algorithms, math, *1500 Ref: https://codeforces.com/contest/1473/submission/104562205

--- codeforces/1473/C.cpp
+++ codeforces/1473/C.cpp
@@ -53,9 +53,6 @@ typedef multiset<int> MSETI;
 /******** User-defined Function *******/
 void solve()
 {
-    /* ll a, b;
-    sll(a);
-    sll(b); */
     ll n, k;
     cin >> n >> k;
     ll u = 2 * k - n - 1;
