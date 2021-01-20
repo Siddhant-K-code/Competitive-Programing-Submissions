d927c259a814e55b2b4d9e0c52dcde3d8761a40c
Siddhant <siddhantkhare2694@gmail.com>
2021-01-20 11:18:00 +0530

    Add solution for problem `D - Cleaning` Link: https://codeforces.com/contest/1474/problem/D Tags: data structures, dp, greedy, math, *2200 Ref: https://codeforces.com/contest/1474/submission/104873854

--- README.md
+++ README.md
@@ -12,13 +12,14 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-33 | [C - Nezzar and Symmetric Array](https://codeforces.com/contest/1478/problem/C) | [GNU C++17 (64)](./codeforces/1478/C.cpp) | `implementation` `math` `sortings` `*1700` | Jan/28/2021 21:35 | 
-32 | [D - Nezzar and Board](https://codeforces.com/contest/1478/problem/D) | [GNU C++17 (64)](./codeforces/1478/D.cpp) | `constructive algorithms` `math` `number theory` `*1800` | Jan/28/2021 20:54 | 
-31 | [B - Nezzar and Lucky Number](https://codeforces.com/contest/1478/problem/B) | [GNU C++17 (64)](./codeforces/1478/B.cpp) | `brute force` `dp` `greedy` `math` `*1100` | Jan/28/2021 20:37 | 
-30 | [A - Nezzar and Colorful Balls](https://codeforces.com/contest/1478/problem/A) | [GNU C++17 (64)](./codeforces/1478/A.cpp) | `brute force` `greedy` `*800` | Jan/28/2021 20:10 | 
-29 | [A - Odd Divisor](https://codeforces.com/contest/1475/problem/A) | [GNU C++17 (64)](./codeforces/1475/A.cpp) | `math` `number theory` `*900` | Jan/26/2021 15:46 | 
-28 | [C - Ball in Berland](https://codeforces.com/contest/1475/problem/C) | [GNU C++17](./codeforces/1475/C.cpp) | `combinatorics` `graphs` `math` `*1400` | Jan/25/2021 22:15 | 
-27 | [B - New Year's Number](https://codeforces.com/contest/1475/problem/B) | [GNU C++17](./codeforces/1475/B.cpp) | `brute force` `dp` `math` `*900` | Jan/25/2021 20:42 | 
+34 | [C - Nezzar and Symmetric Array](https://codeforces.com/contest/1478/problem/C) | [GNU C++17 (64)](./codeforces/1478/C.cpp) | `implementation` `math` `sortings` `*1700` | Jan/28/2021 21:35 | 
+33 | [D - Nezzar and Board](https://codeforces.com/contest/1478/problem/D) | [GNU C++17 (64)](./codeforces/1478/D.cpp) | `constructive algorithms` `math` `number theory` `*1800` | Jan/28/2021 20:54 | 
+32 | [B - Nezzar and Lucky Number](https://codeforces.com/contest/1478/problem/B) | [GNU C++17 (64)](./codeforces/1478/B.cpp) | `brute force` `dp` `greedy` `math` `*1100` | Jan/28/2021 20:37 | 
+31 | [A - Nezzar and Colorful Balls](https://codeforces.com/contest/1478/problem/A) | [GNU C++17 (64)](./codeforces/1478/A.cpp) | `brute force` `greedy` `*800` | Jan/28/2021 20:10 | 
+30 | [A - Odd Divisor](https://codeforces.com/contest/1475/problem/A) | [GNU C++17 (64)](./codeforces/1475/A.cpp) | `math` `number theory` `*900` | Jan/26/2021 15:46 | 
+29 | [C - Ball in Berland](https://codeforces.com/contest/1475/problem/C) | [GNU C++17](./codeforces/1475/C.cpp) | `combinatorics` `graphs` `math` `*1400` | Jan/25/2021 22:15 | 
+28 | [B - New Year's Number](https://codeforces.com/contest/1475/problem/B) | [GNU C++17](./codeforces/1475/B.cpp) | `brute force` `dp` `math` `*900` | Jan/25/2021 20:42 | 
+27 | [D - Cleaning](https://codeforces.com/contest/1474/problem/D) | [GNU C++17](./codeforces/1474/D.cpp) | `data structures` `dp` `greedy` `math` `*2200` | Jan/20/2021 11:18 | 
 26 | [B - Sum of Medians](https://codeforces.com/contest/1440/problem/B) | [GNU C++17](./codeforces/1440/B.cpp) | `greedy` `math` `*900` | Dec/23/2020 13:04 | 
 25 | [B - Fair Numbers](https://codeforces.com/contest/1465/problem/B) | [GNU C++17](./codeforces/1465/B.cpp) | `brute force` `number theory` | Dec/20/2020 20:52 | 
 24 | [A - In-game Chat](https://codeforces.com/contest/1465/problem/A) | [GNU C++17](./codeforces/1465/A.cpp) | `implementation` | Dec/20/2020 20:41 | 
---
+++ codeforces/1474/D.cpp
@@ -0,0 +1,147 @@
+/*
+ * Author : Siddhant Khare
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define ll long long
+#define ld long double
+#define arr array
+#define si(t) scanf("%d", &t)
+#define sl(t) scanf("%ld", &t)
+#define sll(t) scanf("%lld", &t)
+#define sc(t) scanf("%c", &t)
+#define ss(t) scanf("%s", &t)
+#define sf(t) scanf("%f", &t)
+#define sa1(t) scanf("%a1", &t)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%ld\n", x)
+#define pll(x) printf("%lld\n", x)
+#define pf(x) printf("%f")
+#define sz(x) (int)(x).size()
+#define mem(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(x) x.begin(), x.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assea2(B <= A && A <= C)
+#define soa2all(x) soa2(all(x))
+#define mp make_pair
+#define pb push_back
+#define PI 3.1415926535897932384626433832795
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+// const int INF = 1e18;
+#define read(type) readInt<type>()
+const double pi = acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<int> VLL;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+/**************************************/
+
+/******** User-defined Function *******/
+
+bool check(vector<int> v)
+{
+    int n = int(v.size());
+    for (int i = 1; i < n; ++i)
+    {
+        v[i] -= v[i - 1];
+        if (v[i] < 0)
+            return false;
+    }
+    return v.back() == 0;
+}
+
+/******** Solution Function *******/
+
+void solve()
+{
+
+    // pll(ans) // long long print
+}
+
+/**************************************/
+
+/********** Main()  function **********/
+signed main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    /*__*/ //double start = clock(); /*__*/
+
+    //_______________________   _______________
+    ll T;
+    sll(T);
+    while (T--)
+    {
+        int n;
+        si(n);
+        VI v;
+        for (int i = 0; i < n; ++i)
+        {
+            int x;
+            scanf("%d", &x);
+            v.pb(x);
+        }
+        if (check(v))
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+        VI l = v, r = v;
+        int k = n - 1;
+        for (int i = n - 2; i >= 0; --i)
+        {
+            k = i;
+            r[i] -= r[i + 1];
+            if (r[i] < 0)
+                break;
+        }
+        if (k < 2 && check({v[1], v[0], r[2]}))
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+        int flag = 0;
+        for (int i = 1; i + 1 < n; ++i)
+        {
+            if (k <= i + 1)
+            {
+                VI tmp = {l[i - 1], v[i + 1], v[i]};
+                if (i + 2 < n)
+                    tmp.pb(r[i + 2]);
+                if (check(tmp))
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            l[i] -= l[i - 1];
+            if (l[i] < 0)
+                break;
+        }
+        if (flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    //______________________________________
+    //cerr << "Time " << (clock() - start) * 1.000000000 / CLOCKS_PER_SEC << setprecision(9) << "s\n";
+}
+/********  Main() Ends Here *************/
