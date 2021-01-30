04ac7f0c7d00905258cb2a1dfd6e6a02f6feaa30
Siddhant <siddhantkhare2694@gmail.com>
2021-01-30 18:35:00 +0530

    Add solution for problem `D - Staircase Sequences` Link: https://atcoder.jp/contests/abc190/tasks/abc190_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc190/submissions/19808932

---
+++ atcoder/abc190/D.cpp
@@ -0,0 +1,139 @@
+/*
+ * Author : Siddhant Khare
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/*******  All Required define Pre-Processors for Optimizations *******/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+/**************************************/
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define si(t) scanf("%d", &t)
+#define sl(t) scanf("%ld", &t)
+#define sll(t) scanf("%lld", &t)
+#define sc(t) scanf("%c", &t)
+#define ss(t) scanf("%s", &t)
+#define sf(t) scanf("%f", &t)
+#define slf(t) scanf("%lf", &t)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%ld\n", x)
+#define pll(x) printf("%lld\n", x)
+#define pf(x) printf("%f")
+#define sz(x) (int)(x).size()
+#define mem(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(x) x.begin(), x.end()
+#define max_3(a, b, c) max(a, max(b, c))
+#define min_3(a, b, c) min(a, min(b, c))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define sortall(x) sort(all(x))
+#define mp make_pair
+#define pb push_back
+#define PI 3.1415926535897932384626433832795
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define read(type) readInt<type>()
+const double pi = acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<ll> VLL;
+typedef vector<string> VS;
+typedef vector<PII> VPI;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+/**************************************/
+
+/******** User-defined Function *******/
+
+ll n, sum;
+
+ll check(ll x, ll y)
+{
+    ll ans = 0;
+    if (y & 1)
+        ans = ((y + 1) / 2) * y;
+    else
+        ans = (y / 2) * (y + 1);
+
+    x--;
+    if (x & 1)
+        ans -= ((x + 1) / 2) * x;
+    else
+        ans -= (x / 2) * (x + 1);
+
+    return ans;
+}
+bool solve(ll num)
+{
+    ll l = 1, r = n / num + 1;
+    while (l <= r)
+    {
+        ll mid = (l + r) / 2;
+        ll total = check(mid, mid + num - 1);
+
+        if (total == n)
+            return 1;
+        else if (total > n)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return 0;
+}
+/* void solve()
+{
+
+    // Algorithm
+
+    // Output
+    // pll(ans) // long long print
+} */
+
+/**************************************/
+
+/********** Main()  function **********/
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    /*__*/ double start = clock(); /*__*/
+
+    //______________________________________
+    cin >> n;
+    sum = 0;
+    for (ll i = 1; i <= 1e7; i++)
+    {
+        if (solve(i))
+        {
+            sum += 2;
+        }
+    }
+    cout << sum << "\n";
+    return 0;
+    //______________________________________
+    cerr << "Time " << (clock() - start) * 1.0 / CLOCKS_PER_SEC << setprecision(5) << "s\n";
+}
+/********  Main() Ends Here *************/
